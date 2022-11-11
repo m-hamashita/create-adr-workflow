8ad74a7fbf8d7446f75005600110585542588cb4
m-hamashita <masakatsu.hamashita@gunosy.com>
2022-11-11 15:32:10 +0900

    Add workflow

--- create-discussion.sh
+++ create-discussion.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export title=$(sh sample_title.sh)
+export title=$(sh sample_title.sh $1)
 export body=$(cat sample_discussion_body)
 gh api graphql -f "body=$body" -f "title=$title" -f query='mutation CreateDiscussion($title: String!, $body: String!){
       createDiscussion(input: {repositoryId: "R_kgDOIXQxIg", categoryId: "DIC_kwDOIXQxIs4CSWZz", body: $body, title: $title}) {
