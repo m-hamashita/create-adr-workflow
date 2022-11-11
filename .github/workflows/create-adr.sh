9fe05402d2d6fdca0ed2ef827bc44a66f3da1ead
m-hamashita <masakatsu.hamashita@gunosy.com>
2022-11-11 23:43:27 +0900

    Update

--- .github/workflows/create-adr.sh
+++ .github/workflows/create-adr.sh
@@ -8,10 +8,17 @@ ADRNUM=$(gh api graphql --jq '.data.repository.discussions.nodes[].title' -f que
          }
        }
      }
- }'| sed -e 's/ADR \([0-9]*\).*/\1/' | awk '$0>x{x=$0};END{print x+1}')
+ }'| grep ADR | sed -e 's/ADR \([0-9]*\).*/\1/' | awk '$0>x{x=$0};END{print x+1}')
 
 title="ADR "$ADRNUM". "$1
-body=$(cat ./.github/workflows/sample_discussion_body)
+body=$(gh api graphql --jq '.data.repository.discussion.body' -f query='query {
+      repository(owner: "m-hamashita", name: "study-github-actions") {
+        discussion(number: 50) {
+          body
+        }
+      }
+    }'
+)
 gh api graphql -f "body=$body" -f "title=$title" -f query='mutation CreateDiscussion($title: String!, $body: String!){
       createDiscussion(input: {repositoryId: "R_kgDOIXQxIg", categoryId: "DIC_kwDOIXQxIs4CSWZz", body: $body, title: $title}) {
 
