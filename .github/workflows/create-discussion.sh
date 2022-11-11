e63cea4d3a1c323cefe388f6811a01e76b2835d8
m-hamashita <masakatsu.hamashita@gunosy.com>
2022-11-11 15:44:12 +0900

    Update

--- .github/workflows/create-discussion.sh
+++ .github/workflows/create-discussion.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-export title=$(sh sample_title.sh $1)
-export body=$(cat sample_discussion_body)
+ADRNUM=$(gh api graphql --jq '.data.repository.discussions.nodes[].title' -f query='query {
+     repository(owner: "m-hamashita", name: "study-github-actions") {
+       discussions(first: 10) {
+         nodes {
+           title
+         }
+       }
+     }
+ }'| grep "ADR" | sed -e 's/ADR \([0-9]*\).*/\1/' | awk '$0>x{x=$0};END{print x+1}')
+
+title="ADR "$ADRNUM". "$1
+body=$(cat ./.github/workflows/sample_discussion_body)
 gh api graphql -f "body=$body" -f "title=$title" -f query='mutation CreateDiscussion($title: String!, $body: String!){
       createDiscussion(input: {repositoryId: "R_kgDOIXQxIg", categoryId: "DIC_kwDOIXQxIs4CSWZz", body: $body, title: $title}) {
 
