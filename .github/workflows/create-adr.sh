81808c6c26d1d7ae3e3c45ebe1da4b536bf147ae
m-hamashita <masakatsu.hamashita@gunosy.com>
2022-11-14 22:06:31 +0900

    change CategoryID to ADR

--- .github/workflows/create-adr.sh
+++ .github/workflows/create-adr.sh
@@ -20,9 +20,7 @@ body=$(gh api graphql --jq '.data.repository.discussion.body' -f query='query {
     }'
 )
 gh api graphql -f "body=$body" -f "title=$title" -f query='mutation CreateDiscussion($title: String!, $body: String!){
-      createDiscussion(input: {repositoryId: "R_kgDOIXQxIg", categoryId: "DIC_kwDOIXQxIs4CSWZz", body: $body, title: $title}) {
-
-        # response type: CreateDiscussionPayload
+      createDiscussion(input: {repositoryId: "R_kgDOIXQxIg", categoryId: "DIC_kwDOIXQxIs4CSjG2", body: $body, title: $title}) {
         discussion {
           id
         }
