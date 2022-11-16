0f00e24cd0d5e3a5eff3fb80524dee20390a99f6
m-hamashita <masakatsu.hamashita@gunosy.com>
2022-11-16 15:21:18 +0900

    Refactor

--- .github/workflows/create-adr.sh
+++ .github/workflows/create-adr.sh
@@ -4,49 +4,45 @@ REPOSITORY_OWNER="m-hamashita"
 REPOSITORY_NAME="create-adr-workflow"
 TEMPLATE_ID=57
 
-repository_id=$(gh api graphql --jq '.data.repository.id' -f query='query {
-      repository(owner: "'$REPOSITORY_OWNER'", name: "'$REPOSITORY_NAME'") {
+repository_id=$(gh api graphql --jq '.data.repository.id' -f query='{
+    repository(owner: "'$REPOSITORY_OWNER'", name: "'$REPOSITORY_NAME'") {
+        id
+    }
+  }'
+)
+category_id=$(gh api graphql --jq '.data.repository.discussionCategory.id' -f query='{
+    repository(owner: "'$REPOSITORY_OWNER'", name: "'$REPOSITORY_NAME'") {
+      discussionCategory(slug: "ADR") {
         id
       }
-    }'
+    }
+  }'
 )
-category_id=$(gh api graphql --jq '.data.repository.discussionCategories.nodes[] | select(.name=="ADR") | .id' -f query='
-        {
-          repository(owner: "'$REPOSITORY_OWNER'", name: "'$REPOSITORY_NAME'") {
-            discussionCategories(first: 10) {
-              nodes {
-                id
-                name
-              }
-            }
-          }
-        }'
-    )
 
-adr_num=$(gh api graphql --jq '.data.repository.discussions.nodes[].title' -f query='query {
-     repository(owner: "'$REPOSITORY_OWNER'", name: "'$REPOSITORY_NAME'") {
-       discussions(first: 10, orderBy:{field:CREATED_AT, direction: DESC}) {
-         nodes {
-           title
-         }
-       }
-     }
- }'| grep ADR | sed -e 's/ADR \([0-9]*\).*/\1/' | awk '$0>x{x=$0};END{print x+1}')
-
-title="ADR "$adr_num". "$1
-body=$(gh api graphql --jq '.data.repository.discussion.body' -f query='query {
-      repository(owner: "'$REPOSITORY_OWNER'", name: "'$REPOSITORY_NAME'") {
-        discussion(number: '$TEMPLATE_ID') {
-          body
+adr_num=$(gh api graphql --jq '.data.repository.discussions.nodes[].title' -f query='{
+    repository(owner: "'$REPOSITORY_OWNER'", name: "'$REPOSITORY_NAME'") {
+      discussions(first: 10, orderBy:{field:CREATED_AT, direction: DESC}) {
+        nodes {
+          title
         }
       }
-    }'
+    }
+  }'| grep ADR | sed -e 's/ADR \([0-9]*\).*/\1/' | awk '$0>x{x=$0};END{print x+1}')
+
+title="ADR "$adr_num". "$1
+body=$(gh api graphql --jq '.data.repository.discussion.body' -f query='{
+    repository(owner: "'$REPOSITORY_OWNER'", name: "'$REPOSITORY_NAME'") {
+      discussion(number: '$TEMPLATE_ID') {
+          body
+      }
+    }
+  }'
 )
 
 gh api graphql -f "body=$body" -f "title=$title" -f query='mutation CreateDiscussion($title: String!, $body: String!){
     createDiscussion(input: {repositoryId: "'$repository_id'", categoryId: "'$category_id'", body: $body, title: $title}) {
-        discussion {
+      discussion {
           id
-        }
       }
-    }'
+    }
+  }'
