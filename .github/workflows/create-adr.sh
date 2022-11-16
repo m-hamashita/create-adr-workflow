73060ff0c7685d79ee260403304346947f18336a
m-hamashita <masakatsu.hamashita@gunosy.com>
2022-11-16 14:26:05 +0900

    Refactor create adr script

--- .github/workflows/create-adr.sh
+++ .github/workflows/create-adr.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 
-ADRNUM=$(gh api graphql --jq '.data.repository.discussions.nodes[].title' -f query='query {
-     repository(owner: "m-hamashita", name: "study-github-actions") {
-       discussions(first: 10) {
+REPOSITORY_OWNER="m-hamashita"
+REPOSITORY_NAME="create-adr-workflow"
+TEMPLATE_ID=57
+
+repository_id=$(gh api graphql --jq '.data.repository.id' -f query='query {
+      repository(owner: "'$REPOSITORY_OWNER'", name: "'$REPOSITORY_NAME'") {
+        id
+      }
+    }'
+)
+category_id=$(gh api graphql --jq '.data.repository.discussionCategories.nodes[] | select(.name=="ADR") | .id' -f query='
+        {
+          repository(owner: "'$REPOSITORY_OWNER'", name: "'$REPOSITORY_NAME'") {
+            discussionCategories(first: 10) {
+              nodes {
+                id
+                name
+              }
+            }
+          }
+        }'
+    )
+
+adr_num=$(gh api graphql --jq '.data.repository.discussions.nodes[].title' -f query='query {
+     repository(owner: "'$REPOSITORY_OWNER'", name: "'$REPOSITORY_NAME'") {
+       discussions(first: 10, orderBy:{field:CREATED_AT, direction: DESC}) {
          nodes {
            title
          }
@@ -10,17 +33,18 @@ ADRNUM=$(gh api graphql --jq '.data.repository.discussions.nodes[].title' -f que
      }
  }'| grep ADR | sed -e 's/ADR \([0-9]*\).*/\1/' | awk '$0>x{x=$0};END{print x+1}')
 
-title="ADR "$ADRNUM". "$1
+title="ADR "$adr_num". "$1
 body=$(gh api graphql --jq '.data.repository.discussion.body' -f query='query {
-      repository(owner: "m-hamashita", name: "study-github-actions") {
-        discussion(number: 57) {
+      repository(owner: "'$REPOSITORY_OWNER'", name: "'$REPOSITORY_NAME'") {
+        discussion(number: '$TEMPLATE_ID') {
           body
         }
       }
     }'
 )
+
 gh api graphql -f "body=$body" -f "title=$title" -f query='mutation CreateDiscussion($title: String!, $body: String!){
-      createDiscussion(input: {repositoryId: "R_kgDOIXQxIg", categoryId: "DIC_kwDOIXQxIs4CSjG2", body: $body, title: $title}) {
+    createDiscussion(input: {repositoryId: "'$repository_id'", categoryId: "'$category_id'", body: $body, title: $title}) {
         discussion {
           id
         }
